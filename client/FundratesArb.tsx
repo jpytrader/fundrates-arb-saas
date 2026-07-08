fed90f22e51e1cbb6cfd4e4644eea342c2a535ca
jpytrader <fortunerise007@gmail.com>
2026-07-08 14:22:34 +0100

    Optimize Dashboard UI: headerSlot

--- client/FundratesArb.tsx
+++ client/FundratesArb.tsx
@@ -66,7 +66,7 @@ export function index() {
           height: '32px'
         }}
       >
-        <SubscriptionIcon size={16}/>
+        <SubscriptionIcon size={14}/>
       </button>
       <button
         type="button"
@@ -93,7 +93,7 @@ export function index() {
           <>Signing out...</>
         ) : (
           <>
-            <LogoutIcon size={16} />
+            <LogoutIcon size={14} />
           </>
         )}
       </button>
